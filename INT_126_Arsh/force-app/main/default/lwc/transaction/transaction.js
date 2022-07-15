2f0b0a6f02955480f5ac1fc6d4715450ff8cd9d2
Arsh Bandlish <arsh.bandlish@cybergindia.local>
2022-07-15 15:26:19 +0530

    second Commit

---
+++ INT_126_Arsh/force-app/main/default/lwc/transaction/transaction.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class TransactionLWC extends LightningElement {
+greeting = 'World';
+
+changeHandler(event) {
+
+this.greeting = event.target.value;
+
+ }
+}
